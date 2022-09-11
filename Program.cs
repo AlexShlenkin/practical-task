9f2a452a80e05f9965a89bdfdb4d351dbd3c3007
unknown <shlenkin91@gmail.com>
2022-09-11 19:26:24 +0300

    practical task3

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+//Console.WriteLine("Введите первое число");
+//int number =Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите второе число");
+//int number2 =Convert.ToInt32(Console.ReadLine());
+
+//if ( number > number2)
+
+//{
+//Console.WriteLine (number);
+//}
+//else
+//{
+//Console.WriteLine (number2);
+//}
+
+
+Console.WriteLine("Введите первое число");
+int number =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int number3 =Convert.ToInt32(Console.ReadLine());
+int max =number;
+
+if ( max < number2)
+
+max =number2;
+
+if ( max < number3)
+
+max =number3;
+
+{
+Console.WriteLine (max);
+}
+
+
+
+
+
+
+
