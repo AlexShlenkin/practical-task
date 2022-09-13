4642f7beaa4e831affb6f51399236514d96984fc
unknown <shlenkin91@gmail.com>
2022-09-13 23:03:26 +0300

    practical task8

--- Program.cs
+++ Program.cs
@@ -15,30 +15,35 @@
 //}
 
 
-Console.WriteLine("Введите первое число");
-int number =Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите первое число");
+//int number =Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число");
-int number2 =Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите второе число");
+//int number2 =Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите третье число");
-int number3 =Convert.ToInt32(Console.ReadLine());
-int max =number;
+//Console.WriteLine("Введите третье число");
+//int number3 =Convert.ToInt32(Console.ReadLine());
+//int max =number;
 
-if ( max < number2)
+//if ( max < number2)
 
-max =number2;
+//max =number2;
 
-if ( max < number3)
+//if ( max < number3)
 
-max =number3;
+//max =number3;
 
-{
-Console.WriteLine (max);
-}
+//{
+//Console.WriteLine (max);
+//}
 
+int nam = new Random (). Next (1,10) ;
+int nam1 = new Random (). Next (1,10) ;
+int nam2 = new Random (). Next (1,10) ;
+int max = nam+nam1+nam2;  
 
-
+//Console.WriteLine(nam);
+Console.WriteLine(max);
 
 
 
