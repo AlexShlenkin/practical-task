462e6287b82adfbca153dc721896f50c1f52c86b
unknown <shlenkin91@gmail.com>
2022-09-18 09:56:37 +0300

    внесены файлы в gitignor

--- Program.cs
+++ Program.cs
@@ -57,7 +57,50 @@
 //string num = Console.ReadLine();
 
 //Console.WriteLine(num[2]);
+
+//Console.WriteLine("Введите число");
+//int num =Convert.ToInt32(Console.ReadLine());
+
+//if  (num < 1 || num > 7) ;
+
+//if (num==1)
+//{
+  //Console.WriteLine("Понедельник");
+//}
+//if (num==2)
+//{
+//  Console.WriteLine("Вторник");
+//}
+//if (num==3)
+//{
+  //Console.WriteLine("Среда");
+//}
+//if (num==4)
+//{
+  //Console.WriteLine("Четверг");
+//}
+//if (num==5)
+//{
+  //Console.WriteLine("Пятница");
+//}
+//if (num==6)
+//{
+ // Console.WriteLine("Суббота");
+//}
+//if (num==7)
+//{
+  ///Console.WriteLine("Воскрксенье");
+//}
+  //  else
+   // {
+     //   Console.WriteLine("Ошибка");
+
+Console.WriteLine("Введите число");
     
+
+
+
+
 
 
 
